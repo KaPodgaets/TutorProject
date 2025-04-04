c8360c6a68022f0a08d4b715bacb90e1e74c5fec
Kosta P <kapodgaets@gmail.com>
2025-04-04 14:50:06 +0300

    exception middleware handler added

--- TutorProject/src/TutorProject.Web/Program.cs
+++ TutorProject/src/TutorProject.Web/Program.cs
@@ -19,6 +19,8 @@ public static class Program
 
         var app = builder.Build();
 
+        app.UseExceptionCustomHandler();
+
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
