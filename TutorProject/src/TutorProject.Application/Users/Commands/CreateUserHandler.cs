dc17e76852f35cfedfafbcf192a9c16ac19b22f4
Kosta P <kapodgaets@gmail.com>
2025-04-06 09:44:05 +0300

    change dir for projects (stupidity)

---
+++ TutorProject/src/TutorProject.Application/Users/Commands/CreateUserHandler.cs
@@ -0,0 +1,43 @@
+using CSharpFunctionalExtensions;
+using TutorProject.Domain.Shared.Abstractions;
+using TutorProject.Domain.Shared.Errors;
+using TutorProject.Domain.Users;
+
+namespace TutorProject.Application.Users.Commands;
+
+public class CreateUserHandler : ICommandHandler<Guid, CreateUserCommand>
+{
+    private readonly IUsersRepository _usersRepository;
+    private readonly CreateUserValidator _validator;
+
+    public CreateUserHandler(
+        IUsersRepository usersRepository,
+        CreateUserValidator validator)
+    {
+        _usersRepository = usersRepository;
+        _validator = validator;
+    }
+
+    public async Task<Result<Guid, ErrorList>> ExecuteAsync(
+        CreateUserCommand command,
+        CancellationToken cancellationToken = default)
+    {
+        // validation inputs
+        var validationResult = await _validator.ValidateAsync(command, cancellationToken);
+        if (!validationResult.IsValid)
+        {
+            return validationResult.ToErrorList();
+        }
+
+        // business logic validation
+
+        // create new domain entity
+        var newUser = User.CreateUser(command.Email, command.Password).Value;
+
+        // use repository + transaction
+        var result = await _usersRepository.Create(newUser);
+        return result.IsFailure
+            ? result
+            : result.Value;
+    }
+}
---
+++ TutorProject/src/TutorProject.Application/Users/IUsersRepository.cs
@@ -0,0 +1,16 @@
+using CSharpFunctionalExtensions;
+using TutorProject.Domain.Shared.Errors;
+using TutorProject.Domain.Users;
+
+namespace TutorProject.Application.Users;
+
+public interface IUsersRepository
+{
+    Task<Result<Guid, ErrorList>> Create(User newUser);
+
+    Task<Result<Guid, ErrorList>> Delete(User user);
+
+    Task<Result<User, ErrorList>> Update(User user);
+
+    Task<Result<User, ErrorList>> GetById(Guid id);
+}
